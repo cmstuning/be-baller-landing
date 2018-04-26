e27e519cafcc8088e5cec876f4b41e9afa896d51
cmstuning <cmstuning@gmail.com>
2018-04-26 13:50:47 +0300

    added sine-waves

--- my.js
+++ my.js
@@ -26,7 +26,14 @@
  * Check it out at https://github.com/isuttell/sine-waves
  */
 function SineWaveGenerator(options) {
-    $.extend(this, options || {});
+  var ext = function(){
+    for(var i=1; i<arguments.length; i++)
+        for(var key in arguments[i])
+            if(arguments[i].hasOwnProperty(key))
+                arguments[0][key] = arguments[i][key];
+    return arguments[0];
+}
+     ext(this, options || {});
     
     if(!this.el) { throw "No Canvas Selected"; }
     this.ctx = this.el.getContext('2d');

424c2867cb8bddda5e4eac4cdd402b9747db760f
cmstuning <cmstuning@gmail.com>
2018-05-11 11:41:34 +0300

    updated timer

--- js/timer.js
+++ js/timer.js
@@ -3,12 +3,15 @@
 (function () { 
   var launchDate = new Date('2018-09-01T00:00:00').getTime();
   function Timer() {
+    addZero = function (num) {
+      return (num < 10) ? "0"+num : num
+    }
 
     var timeToGo = launchDate - Date.now();
  
-    document.getElementById("seconds").innerHTML = Math.floor((timeToGo % (1000 * 60)) / 1000);
-    document.getElementById("minutes").innerHTML = Math.floor((timeToGo % (1000 * 60 * 60)) / (1000 * 60));
-    document.getElementById("hours").innerHTML = Math.floor((timeToGo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    document.getElementById("seconds").innerHTML = addZero(Math.floor((timeToGo % (1000 * 60)) / 1000));
+    document.getElementById("minutes").innerHTML = addZero(Math.floor((timeToGo % (1000 * 60 * 60)) / (1000 * 60)));
+    document.getElementById("hours").innerHTML = addZero(Math.floor((timeToGo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
     document.getElementById("days").innerHTML = Math.floor(timeToGo / (1000 * 60 * 60 * 24));
 
   }
